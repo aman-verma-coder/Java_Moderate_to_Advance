91da8405f668686f7d160b888d41731d6cd65d6e
Aman Verma <9330nama@gmail.com>
2024-07-12 16:27:30 +0530

    _21p6_chechPalindromeString.java

---
+++ _21p6_chechPalindromeString.java
@@ -0,0 +1,20 @@
+public class _21p6_chechPalindromeString {
+    public static void checkPalindromeString(String str) {
+        boolean result = true;
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
+                result = false;
+            }
+        }
+        if (result == true) {
+            System.out.println("Palindrome String");
+        } else {
+            System.out.println("Not a Palindrome String");
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "racecar";
+        checkPalindromeString(str);
+    }
+}
