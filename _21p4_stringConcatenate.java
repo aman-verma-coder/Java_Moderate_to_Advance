9d19ef22579745ee04160e805dbbf0da5630a392
Aman Verma <9330nama@gmail.com>
2024-07-12 16:01:00 +0530

    _21p4_stringConcatenate.java

---
+++ _21p4_stringConcatenate.java
@@ -0,0 +1,8 @@
+public class _21p4_stringConcatenate {
+    public static void main(String[] args) {
+        String str1 = "Aman";
+        String str2 = "Verma";
+        String fullName = str1 + " " + str2;
+        System.out.println(fullName);
+    }
+}
