c2857107db23c77308f99dc85adffe5296acfb5a
Aman Verma <9330nama@gmail.com>
2024-07-12 15:54:22 +0530

    _21p3_stringLength.java

---
+++ _21p3_stringLength.java
@@ -0,0 +1,6 @@
+public class _21p3_stringLength {
+    public static void main(String[] args) {
+        String str = "Aman Verma";
+        System.out.println("Length of " + "'" + str + "'" + " is " + str.length());
+    }
+}
