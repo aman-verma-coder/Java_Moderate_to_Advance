cc91b0bb14f01bf1aee1589fbb5b5e0ec03d6c86
Aman Verma <9330nama@gmail.com>
2024-08-02 23:18:46 +0530

    _23p21_classAndObjects.java

---
+++ _23p21_classAndObjects.java
@@ -0,0 +1,26 @@
+public class _23p21_classAndObjects {
+    public static void main(String[] args) {
+        Pen pen1 = new Pen();// Created a new pen object named as pen1
+        pen1.setColor("Green");
+        System.out.println(pen1.color);
+        pen1.setTip(6);
+        System.out.println(pen1.tip);
+        pen1.setColor("Blue");
+        System.out.println(pen1.color);
+        pen1.color = "Yellow";
+        System.out.println(pen1.color);
+    }
+}
+
+class Pen {
+    String color;
+    int tip;
+
+    void setColor(String newColor) {
+        color = newColor;
+    }
+
+    void setTip(int newSTip) {
+        tip = newSTip;
+    }
+}
