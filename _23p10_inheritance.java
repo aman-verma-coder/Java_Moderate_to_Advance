415998a74d8b1a493ef2de2eca2b9e0c88dabbf4
Aman Verma <9330nama@gmail.com>
2024-08-05 11:16:08 +0530

    _23p10_inheritance.java

---
+++ _23p10_inheritance.java
@@ -0,0 +1,39 @@
+public class _23p10_inheritance {
+    public static void main(String[] args) {
+        Dog d1 = new Dog();
+        d1.eat();
+        d1.sleep();
+        d1.breath();
+        d1.bark();
+        d1.noOfLegs(4);
+    }
+}
+
+class Animal {
+    String color;
+
+    void eat() {
+        System.out.println("eating...");
+    }
+
+    void sleep() {
+        System.out.println("sleeping...");
+    }
+
+    void breath() {
+        System.out.println("breathing...");
+    }
+
+}
+
+class Dog extends Animal {
+    int legs;
+
+    void bark() {
+        System.out.println("barking...");
+    }
+
+    void noOfLegs(int legs) {
+        System.out.println("No. of legs: " + legs);
+    }
+}
