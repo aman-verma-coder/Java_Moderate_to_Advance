894497ee29196994e2d168e3fe3e17db51cffaf6
Aman Verma <9330nama@gmail.com>
2024-08-05 22:12:32 +0530

    _23p18_packagesInJava.java

---
+++ _23p18_packagesInJava.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;//Java Package
+
+public class _23p18_packagesInJava {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner((System.in));
+        int a = sc.nextInt();
+    }
+}
