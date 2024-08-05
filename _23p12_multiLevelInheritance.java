b77bdcd09ceaf111729c027d689e86205d0782c9
Aman Verma <9330nama@gmail.com>
2024-08-05 11:48:54 +0530

    _23p12_multiLevelInheritance.java

---
+++ _23p12_multiLevelInheritance.java
@@ -0,0 +1,32 @@
+public class _23p12_multiLevelInheritance {
+    public static void main(String[] args) {
+        Dog2 tommy = new Dog2();
+        tommy.eat();
+        tommy.breath();
+        tommy.legs = 4;
+        System.out.println("Tommy has " + tommy.legs + " legs");
+        tommy.bark();
+    }
+}
+
+class Animal2 {
+    String color;
+
+    void eat() {
+        System.out.println("Eating...");
+    }
+
+    void breath() {
+        System.out.println("Breathing...");
+    }
+}
+
+class Mammals extends Animal2 {
+    int legs;
+}
+
+class Dog2 extends Mammals {
+    void bark() {
+        System.out.println("Barking...");
+    }
+}
