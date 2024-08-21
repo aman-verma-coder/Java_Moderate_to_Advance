5554be1640149cb02452e0c3873cfb36e999e49e
Aman Verma <9330nama@gmail.com>
2024-08-21 14:17:23 +0530

    _24p3_printNumsInIncreasingOrder.java

---
+++ _24p3_printNumsInIncreasingOrder.java
@@ -0,0 +1,15 @@
+public class _24p3_printNumsInIncreasingOrder {
+    public static void main(String[] args) {
+        int n = 10;
+        increasingNoPrint(n);
+    }
+
+    public static void increasingNoPrint(int n) {
+        if (n == 1) {
+            System.out.print(n + " ");
+        } else {
+            increasingNoPrint(n - 1);
+            System.out.print(n + " ");
+        }
+    }
+}
