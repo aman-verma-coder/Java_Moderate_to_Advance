49415ba88ce9297c78e6f57b817340dee3692a64
Aman Verma <9330nama@gmail.com>
2024-08-29 22:59:08 +0530

    _25p4_friendsPairingProblem.java

---
+++ _25p4_friendsPairingProblem.java
@@ -0,0 +1,18 @@
+public class _25p4_friendsPairingProblem {
+    public static int friendsPair(int n) {
+        if (n == 1 || n == 2) {
+            return n;
+        }
+        // single
+        int fnm1 = friendsPair(n - 1);
+        // pair
+        int fnm2 = (n - 1) * friendsPair(n - 2);
+        return fnm1 + fnm2;
+        // return friendsPair(n - 1) + (n - 1) * friendsPair(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(friendsPair(n));
+    }
+}
