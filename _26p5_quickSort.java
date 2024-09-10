b8b7cd2290e518eb1befa50b6345af9e921eaec3
Aman Verma <9330nama@gmail.com>
2024-09-10 23:03:22 +0530

    _26p5_quickSort.java

--- _26p5_quickSort.java
+++ _26p5_quickSort.java
@@ -1,4 +1,4 @@
-public class _26p4_quickSort {
+public class _26p5_quickSort {
     public static void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
