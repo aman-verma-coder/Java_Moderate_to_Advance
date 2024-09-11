ff77d3b9fa87fdbb5975b3c0bf03fb9fedec01a4
Aman Verma <9330nama@gmail.com>
2024-09-11 22:29:43 +0530

    Update _26p7_sortedAndRotatedArraySearch.java

--- _26p7_sortedAndRotatedArraySearch.java
+++ _26p7_sortedAndRotatedArraySearch.java
@@ -4,19 +4,19 @@ public class _26p7_sortedAndRotatedArraySearch {
             return -1;
         }
         int mid = (ei + si) / 2;
-        if (arr[mid] == target) {
+        if (arr[mid] == target) {// base case
             return mid;
         }
-        if (arr[si] <= arr[mid]) {
-            if (target >= arr[si] && target <= arr[mid]) {
+        if (arr[si] <= arr[mid]) {// mid lies in first line
+            if (target >= arr[si] && target <= arr[mid]) {// mid lies in left side
                 return sortedArraySearch(arr, si, mid, target);
-            } else {
+            } else {// mid lies in right side
                 return sortedArraySearch(arr, mid + 1, ei, target);
             }
-        } else {
-            if (target >= arr[mid] && target <= arr[ei]) {
+        } else {// mid lies in second line
+            if (target >= arr[mid] && target <= arr[ei]) {// mid lies in left side
                 return sortedArraySearch(arr, mid + 1, ei, target);
-            } else {
+            } else {// mid lies in right side
                 return sortedArraySearch(arr, si, mid, target);
             }
         }
