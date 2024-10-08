73a539c47199ffaaa156957868603b9067b57425
Aman Verma <9330nama@gmail.com>
2024-10-08 22:23:11 +0530

    _29p1_introToArrayList.java

---
+++ _29p1_introToArrayList.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;// 1st word of class should be capital
+
+public class _29p1_introToArrayList {// 1st word of class should be capital
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();// 1st word of data type should be capital
+        ArrayList<String> str = new ArrayList<>();// 1st word of data type should be capital
+        ArrayList<Boolean> boo = new ArrayList<>();// 1st word of data type should be capital
+    }
+}
